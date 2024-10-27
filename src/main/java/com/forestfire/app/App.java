c326bb64c8c06e58c10d11213821a079f044fc53
danychaddad <104129506+danychaddad@users.noreply.github.com>
2024-10-27 21:35:17 +0200

    Fixed spring boot run (#11)

--- src/main/java/com/forestfire/app/App.java
+++ src/main/java/com/forestfire/app/App.java
@@ -1,10 +1,13 @@
 package com.forestfire.app;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
