d74df0624922e89c3051d8584b505cb7204801a2
danychaddad <104129506+danychaddad@users.noreply.github.com>
2024-10-29 00:34:17 +0200

    Created test file

---
+++ src/test/java/com/forestfire/app/sensors/SensorReadingControllerIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.forestfire.app.sensors;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SensorReadingControllerIntegrationTest {
+
+    @Test
+    @DisplayName("all() should be invoked by a GET API call to the readings endpoint")
+    void all_should_be_invoked_by_a_get_api_call_to_the_readings_endpoint() {
+    }
+
+    @Test
+    void newReading() {
+    }
+}
