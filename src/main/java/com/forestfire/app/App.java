59477f2c58507bc192a008b1cb8fc62bc9327196
danychaddad <104129506+danychaddad@users.noreply.github.com>
2024-10-28 23:50:57 +0200

    sensorRepo tested to write and read from mongoDB

--- src/main/java/com/forestfire/app/App.java
+++ src/main/java/com/forestfire/app/App.java
@@ -1,12 +1,19 @@
 package com.forestfire.app;
 
+import com.forestfire.app.sensors.SensorReadingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableAutoConfiguration
+@EnableMongoRepositories
 @SpringBootApplication
 public class App {
+
+    @Autowired
+    private SensorReadingRepository sensorRepo;
+
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
