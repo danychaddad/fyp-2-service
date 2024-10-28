c709ffb078bd13d70fdac12b1ecdc07fd00aba2f
danychaddad <104129506+danychaddad@users.noreply.github.com>
2024-10-29 00:30:17 +0200

    GET and POST requests are implemented

--- src/main/java/com/forestfire/app/App.java
+++ src/main/java/com/forestfire/app/App.java
@@ -1,7 +1,5 @@
 package com.forestfire.app;
 
-import com.forestfire.app.sensors.SensorReadingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -9,11 +7,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 @SpringBootApplication
 public class App {
-
-    @Autowired
-    private SensorReadingRepository sensorRepo;
-
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
